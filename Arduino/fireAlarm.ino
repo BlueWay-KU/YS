f1df88655d0e3c695c49bafadd3383fa6827a04b
yeonshin <38985716+yondini@users.noreply.github.com>
2019-02-24 13:30:19 +0000

    Add files via upload

---
+++ Arduino/fireAlarm.ino
@@ -0,0 +1,30 @@
+//analog input
+int gasPin = A0 ;
+int flamePin = A1;
+
+//sensor vaue
+int gas_val;
+int flame_val;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  gas_val = analogRead(gasPin);
+  flame_val = analogRead(flamePin);
+
+  //Serial.print("smoke sensor: ");
+  //Serial.println(gas_val);
+
+  //Serial.print("flame sensor: ");
+  //Serial.println(flame_val);
+
+  if(gas_val>300 | flame_val<800){
+    Serial.println("1");
+  }
+  else{
+    Serial.println("0");
+  }
+  delay(1000);
+}
